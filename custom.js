f20887bbeb40df7c5f16ecad220b5f0178c4904a
Sagar <sagarpayghan3@gmail.com>
2024-10-08 09:02:31 +0530

    first commit

---
+++ custom.js
@@ -0,0 +1,46 @@
+//fetch data 
+
+function fetchData(){
+
+    $.ajax({
+        url: "fetchapi/",
+        method:'GET',
+        dataType:'json',
+        success:  function (data){
+            let tbody= $('#data-table tbody')
+            tbody.empty();
+            data.forEach(function(item) {
+                tbody.append('<tr><td>'+ item.id +'</td><td>'+item.name+'</td><td>'+item.value+'</td></tr>')
+            }
+            );
+           //success end 
+        },
+        error: function(error) {
+            console.log("Error from backend",error)
+        }
+           //ajax end
+})
+
+    //end function fetchData
+}
+
+// fetch data every 5 second
+setInterval(fetchData, 5000);
+
+
+function fetchValues() {
+    $.ajax({
+        url:'fetchvalues/',
+        method:'GET',
+        dataType:'json',
+        success: function(data){
+
+        },
+        error: function(error){
+            console.log("error at fetch value",error)
+        }
+    })
+}
+
+
+
